a975e51203cd713926449beaaa4108d0afe1f6a5
Marios Sofokleous <marios.sofokleous@yandex.com>
2018-02-08 16:19:18 +0200

    Add entire site to the cache

--- docs/sw.js
+++ docs/sw.js
@@ -2,17 +2,30 @@
 
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
-  './css/style.css',
-  './js/site.js'
+  '/',
+  '/css/style.css',
+  '/js/site.js',
+  'classes.html',
+  'data-related.html',
+  'dom.html',
+  'dom-manipulation.html',
+  'events.html',
+  'expressions-and-operators.html',
+  'flow-control.html',
+  'function-related.html',
+  'functions.html',
+  'index.html',
+  'introduction.html',
+  'object-related.html',
+  'objects.html'
 ];
 
 self.addEventListener('install', function(event) {
   // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
   );
 });
