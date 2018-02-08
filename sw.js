b1fed8448dc3561b1634711571facf161f6be01b
Marios Sofokleous <marios.sofokleous@yandex.com>
2018-02-08 18:00:52 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,21 @@
 /* SERVICE WORKER SCRIPT */ 
 
-var CACHE_NAME = 'my-site-cache-v1';
+var cacheName = 'html5tech-cache';
 
-var urlsToCache = [
-  'css/style.css',
-  'js/site.js',
-  'classes.html',
-  'data-related.html',
-  'dom.html',
-  'dom-manipulation.html',
-  'events.html',
-  'expressions-and-operators.html',
-  'flow-control.html',
-  'function-related.html',
-  'functions.html',
-  'index.html',
-  'introduction.html',
-  'object-related.html',
-  'objects.html'
-];
+var filesToCache = [];
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      console.log('Opened cache');
-      return cache.addAll(urlsToCache);
+// When the service worker is registered, an 'install' event is triggered the 
+// first time the user visits the page
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    // Open cache
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      // Takes a list of URLs, then fetches them from the server and adds the 
+      // response to the cache. This method is atomic and if any of the files 
+      // fail, the entire cache step fails
+      return cache.addAll(filesToCache);
     })
   );
 });
