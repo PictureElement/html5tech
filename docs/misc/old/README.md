2a351bb92cc8ed17e83aedd7a58f59906ab9547e
Marios Sofokleous <marios.sofokleous@yandex.com>
2018-02-09 02:21:11 +0200

    Update docs

---
+++ docs/misc/old/README.md
@@ -0,0 +1,83 @@
+## Status
+
+Pending
+
+## Synopsis
+
+This project is an ever-changing website dedicated to front-end fundamentals.
+The primary use of this page is to demonstrate fundamental topics/concepts in
+HTML, CSS, and JacaScript.
+
+## Example
+
+Some examples of topics/concepts related to HTML are "Elements", "Forms", and
+"Input types".
+
+Some examples of topics/concepts related to CSS are "Selectors", "Color", and
+"Box model".
+
+Each topic/concept is an HTML document by itself organized as follows:
+```html
+<html>
+  <body>
+    <!-- Site heading, navigation menu -->
+    <header>
+      ...
+    </header>
+    <main>
+      <!-- Major topic/concept -->
+      <article>
+        <!-- Byline -->
+        <header>
+          ...
+        </header>
+        <!-- Subsection #1 -->
+        <section>
+          ...
+        </section>
+        <!-- Subsection #2 -->
+        <section>
+          ...
+        </section>
+        ...
+        <!-- Subsection #N -->
+        <section>
+          ...
+        </section>
+      </article>
+    </main>
+    <footer>
+      ...
+    </footer>
+  </body>
+</html>
+```
+## Page Samples
+
+As of Apr. 05, 2017 the website looks like:
+
+![sample1 page](samples/sample1.png)
+
+![sample2 page](samples/sample2.png)
+
+## Contribution
+
+Feel free to be part of this project. Create now your own page regarding a
+specific topic/concept or update an existing page by adding a new section. Just
+remember to be consistent with the site's structure.
+
+## Disclaimer
+
+Keep in mind that I am a beginner in this field. Any advice for personal/site
+improvement is more than welcomed.
+
+## License
+
+- Documentation is licensed under the [Creative Commons Attribution-ShareAlike 4.0 International License](https://creativecommons.org/licenses/by-sa/4.0/)
+- Source code is licensed under the [GNU Affero General Public License v3.0](LICENSE)
+
+Please note that the files in the following directories have their own copyright
+and license information:
+- fonts/Arvo
+- fonts/Lato
+- fonts/Roboto_Mono
---
+++ docs/sw.js
@@ -0,0 +1,160 @@
+// SERVICE WORKER SCRIPT
+
+var cacheName = 'html5tech-cache';
+
+// Files to cache
+// Beware that index.html may also be requested as /
+var filesToCache = [
+  '/html5tech/',
+  '/html5tech/index.html',
+  '/html5tech/classes.html',
+  '/html5tech/data-related.html',
+  '/html5tech/dom.html',
+  '/html5tech/dom-manipulation.html',
+  '/html5tech/expressions-and-operators.html',
+  '/html5tech/flow-control.html',
+  '/html5tech/function-related.html',
+  '/html5tech/functions.html',
+  '/html5tech/introduction.html',
+  '/html5tech/object-related.html',
+  '/html5tech/objects.html',
+
+
+  '/html5tech/css/style.css',
+  '/html5tech/css/prettify.css',
+  '/html5tech/css/bootstrap.min.css',
+  '/html5tech/css/font-awesome.min.css',
+
+  '/html5tech/js/site.js',
+  '/html5tech/js/bootstrap.min.js',
+  '/html5tech/js/jquery.min.js',
+  '/html5tech/js/popper.min.js',
+  '/html5tech/js/run_prettify.js',
+
+  '/html5tech/demos/js/2.js',
+  '/html5tech/demos/js/3.js',
+  '/html5tech/demos/js/5.js',
+  '/html5tech/demos/1-1.html',
+  '/html5tech/demos/1-2.html',
+  '/html5tech/demos/2-1.html',
+  '/html5tech/demos/2-2.html',
+  '/html5tech/demos/3.html',
+  '/html5tech/demos/4.html',
+  '/html5tech/demos/5.html',
+  '/html5tech/demos/6-1.html',
+  '/html5tech/demos/6-2.html',
+  '/html5tech/demos/7-1.html',
+  '/html5tech/demos/7-2.html',
+  '/html5tech/demos/8-1.html',
+  '/html5tech/demos/8-2.html',
+  '/html5tech/demos/9-1.html',
+  '/html5tech/demos/9-2.html',
+  '/html5tech/demos/10.html',
+  '/html5tech/demos/11.html',
+  '/html5tech/demos/12-1.html',
+  '/html5tech/demos/12-2.html',
+  '/html5tech/demos/13-1.html',
+  '/html5tech/demos/13-2.html',
+
+  '/html5tech/images/150x100.png',
+  '/html5tech/images/100x100.png',
+  '/html5tech/images/agpl-v3.svg',
+  '/html5tech/images/cc-by-sa-4.svg',
+
+  'fonts/roboto-slab-v7-latin-100.eot',
+  'fonts/roboto-slab-v7-latin-100.eot?#iefix',
+  'fonts/roboto-slab-v7-latin-100.woff2',
+  'fonts/roboto-slab-v7-latin-100.woff',
+  'fonts/roboto-slab-v7-latin-100.ttf',
+  'fonts/roboto-slab-v7-latin-100.svg#RobotoSlab',
+  'fonts/roboto-slab-v7-latin-300.eot',
+  'fonts/roboto-slab-v7-latin-300.eot?#iefix',
+  'fonts/roboto-slab-v7-latin-300.woff2',
+  'fonts/roboto-slab-v7-latin-300.woff',
+  'fonts/roboto-slab-v7-latin-300.ttf',
+  'fonts/roboto-slab-v7-latin-300.svg#RobotoSlab',
+  'fonts/roboto-slab-v7-latin-regular.eot',
+  'fonts/roboto-slab-v7-latin-regular.eot?#iefix',
+  'fonts/roboto-slab-v7-latin-regular.woff2',
+  'fonts/roboto-slab-v7-latin-regular.woff',
+  'fonts/roboto-slab-v7-latin-regular.ttf',
+  'fonts/roboto-slab-v7-latin-regular.svg#RobotoSlab',
+  'fonts/audiowide-v6-latin-regular.eot',
+  'fonts/audiowide-v6-latin-regular.eot?#iefix',
+  'fonts/audiowide-v6-latin-regular.woff2',
+  'fonts/audiowide-v6-latin-regular.woff',
+  'fonts/audiowide-v6-latin-regular.ttf',
+  'fonts/audiowide-v6-latin-regular.svg#Audiowide',
+  'fonts/montserrat-v12-latin-200.eot',
+  'fonts/montserrat-v12-latin-200.eot?#iefix',
+  'fonts/montserrat-v12-latin-200.woff2',
+  'fonts/montserrat-v12-latin-200.woff',
+  'fonts/montserrat-v12-latin-200.ttf',
+  'fonts/montserrat-v12-latin-200.svg#Montserrat',
+  'fonts/montserrat-v12-latin-200italic.eot',
+  'fonts/montserrat-v12-latin-200italic.eot?#iefix',
+  'fonts/montserrat-v12-latin-200italic.woff2',
+  'fonts/montserrat-v12-latin-200italic.woff',
+  'fonts/montserrat-v12-latin-200italic.ttf',
+  'fonts/montserrat-v12-latin-200italic.svg#Montserrat',
+  'fonts/montserrat-v12-latin-300.eot',
+  'fonts/montserrat-v12-latin-300.eot?#iefix',
+  'fonts/montserrat-v12-latin-300.woff2',
+  'fonts/montserrat-v12-latin-300.woff',
+  'fonts/montserrat-v12-latin-300.ttf',
+  'fonts/montserrat-v12-latin-300.svg#Montserrat',
+  'fonts/montserrat-v12-latin-regular.eot',
+  'fonts/montserrat-v12-latin-regular.eot?#iefix',
+  'fonts/montserrat-v12-latin-regular.woff2',
+  'fonts/montserrat-v12-latin-regular.woff',
+  'fonts/montserrat-v12-latin-regular.ttf',
+  'fonts/montserrat-v12-latin-regular.svg#Montserrat'
+];
+
+// When the service worker is registered, an 'install' event is triggered the 
+// first time the user visits the page
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    // Open cache
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      // Takes a list of URLs, then fetches them from the server and adds the 
+      // response to the cache. This method is atomic and if any of the files 
+      // fail, the entire cache step fails
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+// The 'activate' event is fired when the service worker starts up
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  // Logic to update the cache whenever any of the app shell files change
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+// We've cached the app shell components, but we still need to load them from 
+// the local cache.
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    // Evaluate the web request to see if it's available in the cache. It then 
+    // either responds with the cached version, or uses fetch to get a copy from 
+    // the network. The response is passed back to the web page with 
+    // e.respondWith().
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
