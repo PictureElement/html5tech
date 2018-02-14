65f17ac7804342bfc6b1c3232d56531781a29b30
Marios Sofokleous <marios.sofokleous@yandex.com>
2018-02-14 17:19:06 +0200

    Minor fix

--- script.sh
+++ script.sh
@@ -65,12 +65,13 @@ perl -0pi -e "s/js\/output.min.js/\/html5tech\/js\/output.min.js/" dist/*.html
 # step 13
 grunt htmlmin
 
+push("css/prettify.css")
 # step 14
-perl -0pi -e 's/push("css\/prettify.css")/push("\/html5tech\/css\/purestyles.min.css")/g' dist/js/output.min.js
-perl -0pi -e 's/.\/sw.js/\/html5tech\/sw.js/g' dist/js/output.min.js
+perl -0pi -e "s/css\/prettify.css/\/html5tech\/css\/purestyles.min.css/g" dist/js/output.min.js
+perl -0pi -e "s/.\/sw.js/\/html5tech\/sw.js/g" dist/js/output.min.js
 
 # step 15
-perl -0pi -e 's/..\/dist\/fonts/\/html5tech\/fonts/g' dist/*.html
+perl -0pi -e "s/..\/dist\/fonts/\/html5tech\/fonts/g" dist/*.html
 
 #step 16
-perl -0pi -e 's/..\/js\/jquery.min.js/\/html5tech\/js\/output.min.js/' demos/*.html
+perl -0pi -e "s/..\/js\/jquery.min.js/\/html5tech\/js\/output.min.js/" demos/*.html
