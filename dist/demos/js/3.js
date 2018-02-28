f50c62496c99f68112e767bfd7f22b4e5a53dbc9
Marios Sofokleous <marios.sofokleous@yandex.com>
2018-02-28 16:44:59 +0200

    Update canvas-introduction.html. Update sw.js

--- dist/demos/js/3.js
+++
@@ -1,9 +0,0 @@
-// bio object
-var bio = {
-    "name": "Marios Sofokleous",
-    "role": "Web Developer"
-};
-
-// jQuerry's append() method that inserts content to the end of the selected
-// element(s).
-$("#fullName").append(bio.name);
